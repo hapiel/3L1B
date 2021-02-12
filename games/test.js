64eabd880356fb9133a4105e96b29f6df87c2247
Hapiel <daniel@danielsiegmund.nl>
2021-02-12 19:42:02 +0100

    Fn functions work

--- games/test.js
+++ games/test.js
@@ -1,10 +1,13 @@
-const { Board, Led, Leds, Fn } = require("johnny-five");
+const { Board, Led, Leds, Fn, Button } = require("johnny-five");
 const board = new Board();
 
-// Initialisation outside of board so you can controll the led object from the console
 let led1;
 let led2;
-let led3
+let led3;
+let leds;
+
+let button;
+
 board.on("ready", () => {
 
   // Create a standard led component instance
@@ -12,9 +15,18 @@ board.on("ready", () => {
   led2 = new Led(1);
   led3 = new Led(2);
 
+  leds = new Leds([led1,1,2]);
+
+  button = new Button(5);
+
+  // button.on("press", () => {
+  //   leds.stop();
+  // });
+
   // "blink" the led in 500ms
   // on-off phase periods
   led1.blink(100);
   led2.blink(100);
+    // leds.blink();
   // led3.blink(0);
 });
---
+++ games/test2.js
@@ -0,0 +1,4 @@
+const { Board, Led, Leds, Fn, Button } = require("johnny-five");
+
+
+console.log(Fn.range.prefixed("a", 5, 8, 1));
