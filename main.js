8459f0ba9f5833bf4afa88fa81b1c310ecc13a63
Imani Dap <imani.dap@gmail.com>
2021-02-13 20:18:52 +0100

    Move tests to test folder

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 
 // load js file depending on url parameter
-const urlParams = new URLSearchParams(window.location.search);
-const s = document.createElement("script");
-s.type = "text/javascript";
-s.src = "games/" + urlParams.get("game") + ".js";
-document.getElementsByTagName("head")[0].appendChild(s);
+(async function() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const s = document.createElement("script");
+  s.type = "text/javascript";
+  let res = await fetch(`games/${urlParams.get("game")}.js`)
+  if(res.status < 200 || res.status >= 300) {
+    res = await fetch(`tests/${urlParams.get("game")}.js`)
+  }
+  s.src = res.url
+  document.getElementsByTagName("head")[0].appendChild(s);
 
-// dropdown menu
-document.getElementById("game-list").addEventListener("change", (e) =>{
-  // change url parameter to select value
-  window.location.search = "?game=" + e.target.value;
-});
+  // dropdown menu
+  document.getElementById("game-list").addEventListener("change", (e) =>{
+    // change url parameter to select value
+    window.location.search = "?game=" + e.target.value;
+  });
+}());
