d3a41d65133f57b6aa55419286a17b38661701c2
Imani Dap <imani.dap@gmail.com>
2021-03-10 18:16:07 +0100

    switch to bundling with parcel

---
+++ v3/main.js
@@ -0,0 +1,7 @@
+import hljs from 'highlight.js';
+import 'highlight.js/styles/github.css';
+
+window.addEventListener('DOMContentLoaded', () => {
+    //NOTE: the best options are color-brewer & ascetic
+    hljs.highlightAll();
+});
