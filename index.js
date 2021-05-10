13ebb155795762d3be918fb5685d3d52d9ebc77a
Hapiel <daniel@danielsiegmund.nl>
2021-05-10 09:08:07 +0200

    game selection system

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// show and hide source
+
 let showSource = false;
 
 document.querySelectorAll(".source-link").forEach(item => {
@@ -17,3 +19,53 @@ document.querySelectorAll(".source-link").forEach(item => {
     showSource = !showSource;
   });
 });
+
+// load game info
+
+const titleEl = document.getElementById("title");
+const authorEl = document.getElementById("author");
+const instructionsEl = document.getElementById("instructions");
+const sourceEl = document.getElementById("source-code");
+const gameSelectionEl = document.getElementById("game-selection");
+
+// store json data so that changeGame() can access this.
+let gameData;
+
+fetch('games.json')
+  .then(response => response.json())
+  .then(data => {
+
+    // store json data so that changeGame() can access this.
+    gameData = data;
+
+    // open the first game
+    changeGame(0);
+
+    
+
+    //create games list
+    data.games.forEach((game, i) => {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      a.setAttribute("href", "#");
+      a.setAttribute("onclick", "changeGame(" + i + ")");
+      console.log(i);
+      const title = document.createTextNode(game.title);
+      a.appendChild(title);
+
+      li.appendChild(a);
+      gameSelectionEl.append(li);
+      
+    });
+  });
+
+// change the game
+function changeGame(id){
+  titleEl.innerHTML = gameData.games[id].title;
+  authorEl.innerHTML = gameData.games[id].author;
+  instructionsEl.innerHTML = gameData.games[id].instructions;
+  sourceEl.innerHTML = gameData.games[id].source;
+
+  // turn on code highlight
+  hljs.highlightAll();
+}
